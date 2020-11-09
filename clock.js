6ac716502fc428004a0f826395505653e5929d24
hatae <hata676@naver.com>
2020-11-10 08:22:22 +0900

    dsad

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const seconds = 720*3600;
+
+  seconds--;
+  
+  clockTitle.innerText = `${seconds/3600 < 10 ? `0${seconds/3600}` : `${seconds/3600}`}:${
+    seconds%3600/60 < 10 ? `0${seconds%3600/60}` : `${seconds%3600/60}`
+  }:${seconds%3600%60 < 10 ? `0${seconds%3600%60}` : `${seconds%3600%60}`}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
