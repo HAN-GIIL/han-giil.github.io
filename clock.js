c646ec036ae45b42b3581ab6cc7349ea7f5c8ce9
hatae <hata676@naver.com>
2020-11-10 08:24:01 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,9 +2,9 @@ const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
-  const seconds = 720*3600;
+  seconds = 720*3600;
 
-  seconds--;
+  seconds = seconds-1;
   
   clockTitle.innerText = `${seconds/3600 < 10 ? `0${seconds/3600}` : `${seconds/3600}`}:${
     seconds%3600/60 < 10 ? `0${seconds%3600/60}` : `${seconds%3600/60}`
